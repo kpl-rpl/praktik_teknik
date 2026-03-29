c25592354a831624a4e98f186de1173fb2f5250f
Gector <gectorashera@gmail.com>
2026-03-29 17:50:32 +0700

    Add call to JalankanModul in Main method

--- kpl_implementasi_teknik/Program.cs
+++ kpl_implementasi_teknik/Program.cs
@@ -2,7 +2,10 @@
 {
     static void Main(string[] args)
     {
+        KeuanganKos.JalankanModul();
         TeknikStateBased.Run();
         TeknikGeneric.Run();
+        Console.WriteLine("\nTekan tombol untuk keluar...");
+            Console.ReadKey();
     }
 }
